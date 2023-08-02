ae9b434d8b15697c46cda67b3cc8d5917f5f9600
Kuroashi1995 <kurashi.prog@gmail.com>
2023-08-02 11:48:10 -0400

    Improve user message bubble

--- lib/presentation/widgets/chat/message_bubble.dart
+++ lib/presentation/widgets/chat/message_bubble.dart
@@ -9,13 +9,26 @@ class MessageBubble extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
-        Container(
-          decoration: BoxDecoration(
-              color: colors.primary, borderRadius: BorderRadius.circular(10)),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Text("My Message"),
-          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: colors.primary,
+                  borderRadius: BorderRadius.circular(10)),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Text("My dMessage"),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(6, 0, 0, 0),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://cdn.wallpapersafari.com/35/22/Y9WFlq.jpg'),
+              ),
+            )
+          ],
         ),
         const SizedBox(
           height: 10,
