5d0cd2d0c983db394aa63ad0d113a9f9f8221a13
Kuroashi1995 <kurashi.prog@gmail.com>
2023-08-08 09:38:19 -0400

    implement easter egg

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -9,17 +9,36 @@ class ChatProvider extends ChangeNotifier {
     const Message(text: "Hola Adivinator", fromWhom: FromWhom.mine),
     const Message(text: "Estas ahi?", fromWhom: FromWhom.mine),
   ];
+  bool hat = false;
 
   Future<void> sendMessage(String text) async {
     if (text.isEmpty) return;
     final newMessage = Message(text: text, fromWhom: FromWhom.mine);
     messages.add(newMessage);
+    if (text == '*se pone el sombrero*') {
+      hat = true;
+      messages.add(
+        const Message(
+            text: 'PERRY EL ORNITORRINCO?',
+            fromWhom: FromWhom.adivinator,
+            imageUrl:
+                'https://64.media.tumblr.com/1a486a42331daa143eea1f74a94e559c/ba8549d4bd1dd368-28/s1280x1920/4e6ca9f1137031f28e666b45f883e0767aa40f06.gif'),
+      );
+    }
+    if (text.endsWith('?')) {
+      getAdivination();
+    }
 
     notifyListeners();
     scrollToBottom();
   }
 
-  Future<void> getAdivination() {}
+  Future<void> getAdivination() async {
+    messages.add(
+      await yesNoAnswer.getAnswer(),
+    );
+    notifyListeners();
+  }
 
   void scrollToBottom() {
     scrollController.animateTo(scrollController.position.maxScrollExtent,
