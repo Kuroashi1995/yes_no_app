1c012ba3f69770c0e7a4017c1d47fa56de825d59
Kuroashi1995 <kurashi.prog@gmail.com>
2023-08-08 09:37:47 -0400

    implement model with mapper

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -1,10 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/infrastructure/models/yes_no_model.dart';
 
 class YesNoAnswer {
   final _dio = Dio();
-  Future<Message> getAnswer() {
-    final response = _dio.get("https://yesno.wtf/api");
-    return Message(fromWhom: FromWhom.adivinator);
+  Future<Message> getAnswer() async {
+    final response = await _dio.get("https://yesno.wtf/api");
+    return YesNoModel.fromJsonMap(response.data).toMessageEntity();
   }
 }
